0f149982fe8a72043ec5a8c074d5489381baed65
sandhyaganta <gantasandhyavitasoft@gmail.com>
2023-11-14 12:17:05 +0530

    routingpath created section and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SectionComponent } from './section/section.component';
+import  { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "hai",
+    component:SectionComponent,
+  },
+  {
+    path: "hello",
+    component:FooterComponent,
+  }
+ 
+  
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
